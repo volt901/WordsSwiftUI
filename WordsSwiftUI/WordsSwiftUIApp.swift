1374587eb148d8846d3268cb98be2ec7629bbbf7
volt901 <89735853+volt901@users.noreply.github.com>
2023-03-25 19:13:40 +0500

    Initial Commit

---
+++ WordsSwiftUI/WordsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsSwiftUIApp.swift
+//  WordsSwiftUI
+//
+//  Created by Владимир Ширяев on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
